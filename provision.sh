570908109edae1816168dc24f8ace2bd89464c6f
Zaki Juhari <sayhello@zakijuhari.com>
2014-02-02 20:10:19 +0800

    Added provision.sh for Vagrant shell provisioning
    A basic shell provision configuration geared towards a full-stack
    JavaScript development environment.

---
+++ provision.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+<%= import 'vagrant-shell-scripts/ubuntu.sh' %>
+
+# ===============================
+# Setup APT
+# ===============================
+
+# Use a local Ubuntu mirror, results in faster downloads.
+apt-mirror-pick 'sg'
+
+# Custom repositories
+apt-packages-ppa 'chris-lea/node.js'
+apt-packages-repository 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' '7F0CEB10'
+
+# Update packages cache.
+apt-packages-update
+
+# ===============================
+# Install Base Packages.
+# ===============================
+
+apt-packages-install	\
+	git-core			\
+	curl				\
+	build-essential		\
+	openssl 			\
+	libssl-dev			\
+	libfreetype6 		\
+	libfreetype6-dev	\
+	fontconfig				
+
+# ===============================
+# Install NodeJS
+# ===============================
+
+apt-packages-install 	\
+	nodejs				\
+	mongodb-10gen		
+
+# ===============================
+# Install Global Node Package Modules
+# ===============================
+
+npm-packages-install	\
+ 	n 					\
+ 	grunt				\
+ 	bower				\
+	phantomjs 			\
+ 	casperjs			\
+ 	node-sass			\
+ 	http-server			
